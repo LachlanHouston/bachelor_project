2b03f848630079fc308702c54122c83ac15358eb
Lachlan Houston <LachlanHouston111@gmail.com>
2024-01-31 08:45:45 +0100

    Discriminator

--- gan/models/discriminator.py
+++ gan/models/discriminator.py
@@ -1,100 +1,73 @@
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
+import torchaudio
 
 class Conv2DBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=(5, 2), stride=(2, 2), 
-                 padding=(0, 1), d_norm='spectral_norm'):
-        super(Conv2DBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+    def __init__(self, in_channels, out_channels, kernel_size=(3, 2), stride=(2, 1), padding=(0, 0)):
+        super().__init__()
+        norm_f = nn.utils.spectral_norm
+        self.conv = norm_f(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding))
         self.activation = nn.LeakyReLU(0.1)
         nn.init.xavier_uniform_(self.conv.weight)
         nn.init.zeros_(self.conv.bias)
 
-
-
-    def forward(self, x):
+    def forward(self, x) -> torch.Tensor:
         x = self.conv(x)
         x = self.activation(x)
         return x
     
 class Discriminator(nn.Module):
-    def __init__(self, num_conv_layers=6, num_fc_layers=2):
+    def __init__(self, input_sizes=[], output_sizes=[]):
         super(Discriminator, self).__init__()
         self.conv_layers = nn.ModuleList()
-        self.fc_layers = nn.ModuleList()
-        self.num_conv_layers = num_conv_layers
-        self.num_fc_layers = num_fc_layers
-        self.conv_layers.append(Conv2DBlock(2, 8, kernel_size=(5, 5), stride=(2, 2)))
-        self.conv_layers.append(Conv2DBlock(8, 16, kernel_size=(5, 5), stride=(2, 2)))
-        self.conv_layers.append(Conv2DBlock(16, 32, kernel_size=(5, 5), stride=(2, 2)))
-        self.conv_layers.append(Conv2DBlock(32, 64, kernel_size=(5, 5), stride=(2, 2)))
-        self.conv_layers.append(Conv2DBlock(64, 128, kernel_size=(5, 5), stride=(2, 2)))
-        self.conv_layers.append(Conv2DBlock(128, 128, kernel_size=(5, 5), stride=(2, 2)))
-        self.fc_layers.append(nn.Linear(256, 64))
-        self.fc_layers.append(nn.Linear(64, 1))
+        self.input_sizes = input_sizes
+        self.output_sizes = output_sizes
 
-    def forward(self, x):
-        for i in range(self.num_conv_layers):
+        norm_f = nn.utils.spectral_norm
+
+        assert len(self.input_sizes) == len(self.output_sizes), "Input and output sizes must be the same length"
+
+        for i in range(len(self.input_sizes)):
+            self.conv_layers.append(Conv2DBlock(self.input_sizes[i], self.output_sizes[i], kernel_size=(5, 5), stride=(2, 2)))
+            
+        self.fc_layers1  = norm_f(nn.Linear(256, 64))
+        self.activation = nn.LeakyReLU(0.1)
+        self.fc_layers2  = norm_f(nn.Linear(64, 1))
+
+    def forward(self, x) -> torch.Tensor:
+        for i in range(len(self.input_sizes)):
             x = self.conv_layers[i](x)
-            print(i, x.shape)
-        x = torch.flatten(x, 1, -1)
-        print(x.shape)
-        for i in range(self.num_fc_layers):
-            x = self.fc_layers[i](x)
-            print(x.shape)
+        x = x.flatten(1, -1)
+        x = self.fc_layers1(x)
+        x = self.activation(x)
+        x = self.fc_layers2(x)
+        
         return x
     
 
 if __name__ == '__main__':
-    import torchaudio
-    waveform, sample_rate = torchaudio.load('data/clean_raw/p226_004.wav', normalize=True)
-    print(waveform.shape)
+    waveform1, sample_rate1 = torchaudio.load('data/clean_raw/p226_006.wav')
+    #waveform2, sample_rate2 = torchaudio.load('data/clean_raw/p226_009.wav')
+    sample_rate = sample_rate1
+    
+    # Cut into 2 second chunks
+    waveform1 = waveform1[:, :sample_rate*2]
+    #waveform2 = waveform2[:, :sample_rate*2]
+
+    #waveform = torch.cat((waveform1, waveform2), dim=0)
 
     # Downsample to 16 kHz
-    waveform = torchaudio.transforms.Resample(sample_rate, 16000)(waveform)
+    waveform = torchaudio.transforms.Resample(sample_rate, 16000)(waveform1)
     sample_rate = 16000
 
-    if len(waveform.shape) == 3 and waveform.shape[1] == 1:
-        waveform = waveform.squeeze(1)
-        print("yes")
-    
     # Apply STFT
-    Xstft = torch.stft(waveform, n_fft=512, hop_length=100, win_length=400, return_complex=True)
-    data_list = [Xstft.real, Xstft.imag]
-    data = torch.cat(data_list, dim=1)
-    print(data.shape)
+    Xstft = torch.stft(waveform, n_fft=512, hop_length=100, win_length=400, return_complex=True, window=torch.hann_window(400))
+    x = torch.stack((Xstft.real, Xstft.imag), dim=1)
+    print(x.shape)
 
-    x = data
-
-
-    model = Discriminator()
+    model = Discriminator(input_sizes=[2, 8, 16, 32, 64, 128], output_sizes=[8, 16, 32, 64, 128, 128])
     out = model(x)
     print(out.shape)
-
-    # Load file from data/clean_raw/p226_004.wav
-    # import torchaudio
-    # waveform, sample_rate = torchaudio.load('data/clean_raw/p226_004.wav', normalize=True)
-    # print(waveform.shape)
-
-    # # Downsample to 16 kHz
-    # waveform = torchaudio.transforms.Resample(sample_rate, 16000)(waveform)
-    # sample_rate = 16000
-
-    # # Split into 2 second chunks
-    # for i in range(0, waveform.shape[1], 32000):
-    #     chunk = waveform[:, i:i+32000]
-    #     print(chunk.shape)
-
-
-    # # Apply STFT
-    # # spectrogram = torchaudio.transforms.Spectrogram(n_fft=512, hop_length=100, win_length=400)(waveform)
-
-    # # Another way
-    # Xstft = torch.stft(waveform, n_fft=512, hop_length=100, win_length=400, return_complex=True)
-    # print(Xstft.shape)
-    # data_list = [Xstft.real, Xstft.imag]
-    # data = torch.cat(data_list, dim=1)
-    # print(data.shape)
+    print(out)
         
         
