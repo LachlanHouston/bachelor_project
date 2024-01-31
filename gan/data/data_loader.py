d802240d81cbcf888d3544ea16e03e074d282f27
Lachlan Houston <LachlanHouston111@gmail.com>
2024-01-31 11:42:32 +0100

    Data loader first iteration

--- gan/data/data_loader.py
+++ gan/data/data_loader.py
@@ -5,68 +5,65 @@ from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
 
 class AudioDataset(Dataset):
-    def __init__(self, path, new_sample_rate=16000, n_seconds=2):
+    def __init__(self, clean_path, noisy_path, new_sample_rate=16000):
         super(AudioDataset, self).__init__()
-        self.path = path
-        self.files = os.listdir(path)
+        self.clean_path = clean_path
+        self.clean_files = [file for file in os.listdir(clean_path) if file.endswith('.wav')]
+
+        self.noisy_path = noisy_path
+        self.noisy_files = [file for file in os.listdir(noisy_path) if file.endswith('.wav')]
+
         self.new_sample_rate = new_sample_rate
-        self.n_seconds = n_seconds
 
     def __len__(self):
-        return len(self.files)
+        return len(self.clean_files)
     
-    def transform(self, waveform, sample_rate):
-        """Transform the waveform"""
+    def transform(self, waveform, sample_rate, max_length):
         # Downsample to 16 kHz
         waveform = torchaudio.transforms.Resample(sample_rate, self.new_sample_rate)(waveform)
         
-        # Cut into n second chunks
-        chunks = []
-        for i in range(0, waveform.shape[1], self.new_sample_rate * self.n_seconds):
-            chunk = waveform[:, i:i+self.new_sample_rate*self.n_seconds]
-            if chunk.shape[1] != self.new_sample_rate * self.n_seconds:
-                chunk = F.pad(chunk, (0, self.new_sample_rate * self.n_seconds - chunk.shape[1]))
+        # Pad the waveform to have the same length
+        waveform = F.pad(waveform, (0, max_length - waveform.shape[1]))
 
-            chunk_stft = torch.stft(chunk, n_fft=512, hop_length=100, win_length=400, window=torch.hann_window(400), return_complex=True)
-            chunk_stft = torch.stack((chunk_stft.real, chunk_stft.imag), dim=1)
-            chunks.append(chunk_stft)
-        print(len(chunks))
-        return torch.stack(chunks, dim=2)
+        # Process with stft
+        Xstft = torch.stft(waveform, n_fft=512, hop_length=100, win_length=400, window=torch.hamming_window(400), return_complex=True)
+        data_list = [Xstft.real, Xstft.imag]
+        data = torch.cat(data_list, dim=1)
 
-
+        return data
 
     def __getitem__(self, idx, transform=True):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
-        print(self.path + self.files[idx])
         
-        waveform, sample_rate = torchaudio.load(self.path + self.files[idx])
-        stft = None
+        clean_file = self.clean_files[idx]
+        noisy_file = self.noisy_files[idx]
+
+        clean_waveform, clean_sample_rate = torchaudio.load(self.clean_path + clean_file)
+        noisy_waveform, noisy_sample_rate = torchaudio.load(self.noisy_path + noisy_file)
+
+        clean_stft = None
+        noisy_stft = None
+        max_length = 725379
 
         if transform:
-            stft = self.transform(waveform, sample_rate)
+            clean_stft = self.transform(clean_waveform, clean_sample_rate, max_length)
+            noisy_stft = self.transform(noisy_waveform, noisy_sample_rate, max_length)
 
-        return waveform, sample_rate, stft
+        return clean_stft, noisy_stft
     
 def collate_fn(batch):
-    """Collate function for the DataLoader"""
-    waveforms = []
-    sample_rates = []
-    stft = []
-    for waveform, sample_rate, spectrogram in batch:
-        waveforms.append(waveform)
-        sample_rates.append(sample_rate)
-        stft.append(spectrogram)
-    
-    return waveforms, sample_rates, stft
+    clean_stft, noisy_stft = zip(*batch)
+    return clean_stft, noisy_stft
 
 if __name__ == '__main__':
     clean_processed_path = 'data/clean_raw/'
-    clean_dataset = AudioDataset(clean_processed_path)
-    clean_dataloader = DataLoader(clean_dataset, batch_size=2, shuffle=False, collate_fn=collate_fn)
-    for waveforms, sample_rates, spectrograms in clean_dataloader:
-        print(waveforms[1].shape)
-        print(sample_rates[1])
-        print(spectrograms[1].shape)
+    noisy_processed_path = 'data/noisy_raw/'
+    
+    dataset = AudioDataset(clean_processed_path, noisy_processed_path)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
+
+    for i, (clean_stft, noisy_stft) in enumerate(dataloader):
+        print(clean_stft[0].shape)
+        print(noisy_stft[0].shape)
         break
