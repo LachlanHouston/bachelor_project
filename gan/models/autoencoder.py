b603ff2d2b692883c64a8c0a6cf286a61615c39c
Lachlan Houston <LachlanHouston111@gmail.com>
2024-02-13 09:19:44 +0100

    lr scheduler

--- gan/models/autoencoder.py
+++ gan/models/autoencoder.py
@@ -150,12 +150,12 @@ class Autoencoder(L.LightningModule):
         real_clean = batch[0]
         real_noisy = batch[1]
 
-        real_clean = torch.randn(2, 2, 257, 321, device=self.device)
-        real_noisy = torch.randn(2, 2, 257, 321, device=self.device)
+        # real_clean = torch.randn(2, 2, 257, 321, device=self.device)
+        # real_noisy = torch.randn(2, 2, 257, 321, device=self.device)
 
         # Remove tuples and convert to tensors
-        # real_clean = torch.stack(real_clean, dim=1).squeeze(0)
-        # real_noisy = torch.stack(real_noisy, dim=1).squeeze(0)
+        real_clean = torch.stack(real_clean, dim=1).squeeze(0)
+        real_noisy = torch.stack(real_noisy, dim=1).squeeze(0)
 
         fake_clean = self.generator(real_noisy)
 
