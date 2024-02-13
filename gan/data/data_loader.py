79ccc875bde01be76595f4a6d6752980aecb161d
Frederikravnborg <frederik@ravnborg.fr>
2024-02-13 15:53:50 +0100

    Update data_loader.py

--- gan/data/data_loader.py
+++ gan/data/data_loader.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 from scipy.io import wavfile
 
 class AudioDataset(Dataset):
-    def __init__(self, clean_path, noisy_path, normalize=None,
+    def __init__(self, clean_path, noisy_path, standardize=None,
                  new_sample_rate=16000):
         super(AudioDataset, self).__init__()
         self.clean_path = clean_path
@@ -16,7 +16,7 @@ class AudioDataset(Dataset):
         self.noisy_files = [file for file in os.listdir(noisy_path) if file.endswith('.wav')]
 
         self.new_sample_rate = new_sample_rate
-        self.normalize = normalize
+        self.standardize = standardize
 
     def __len__(self):
         return len(self.clean_files)
@@ -27,8 +27,8 @@ class AudioDataset(Dataset):
         clean_waveform, cur_sample_rate = torchaudio.load(self.clean_path + clean_file)
         noisy_waveform, _ = torchaudio.load(self.noisy_path + noisy_file)
 
-        # Normalize the waveforms
-        if self.normalize:
+        # standardize the waveforms
+        if self.standardize:
             clean_waveform = (clean_waveform - clean_waveform.mean()) / (clean_waveform.std() + 1e-8)
             noisy_waveform = (noisy_waveform - noisy_waveform.mean()) / (noisy_waveform.std() + 1e-8)
 
@@ -71,7 +71,7 @@ def waveform_to_stft(waveform):
 def data_loader(clean_path, noisy_path, split =[0.8, 0.1, 0.1],
                 batch_size=16, num_workers=4):
     
-    dataset = AudioDataset(clean_path, noisy_path, normalize=False)
+    dataset = AudioDataset(clean_path, noisy_path, standardize=False)
     train_size = int(split[0] * len(dataset))
     val_size = int(split[1] * len(dataset))
     test_size = len(dataset) - train_size - val_size
