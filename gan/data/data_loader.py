139eb1a65980b4640bdbbd7cee182525b7e711fe
Frederikravnborg <frederik@ravnborg.fr>
2024-02-24 15:28:58 +0100

    implement data caching

--- gan/data/data_loader.py
+++ gan/data/data_loader.py
@@ -2,33 +2,34 @@ import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as L
+from tqdm import tqdm
 
+# with caching:
 class AudioDataset(Dataset):
     def __init__(self, clean_path, noisy_path, standardize=None,
                  new_sample_rate=16000):
         super(AudioDataset, self).__init__()
         self.clean_path = clean_path
-        self.clean_files = [file for file in os.listdir(clean_path) if file.endswith('.pt')]
-
         self.noisy_path = noisy_path
-        self.noisy_files = [file for file in os.listdir(noisy_path) if file.endswith('.pt')]
-
         self.new_sample_rate = new_sample_rate
         self.standardize = standardize
 
+        # Preload data into cache
+        self.data = []
+        clean_files = [file for file in os.listdir(clean_path) if file.endswith('.pt')]
+        noisy_files = [file for file in os.listdir(noisy_path) if file.endswith('.pt')]
+        
+        for clean_file, noisy_file in tqdm(zip(clean_files, noisy_files), desc="Loading data"):
+            clean_stft = torch.load(os.path.join(clean_path, clean_file))
+            noisy_stft = torch.load(os.path.join(noisy_path, noisy_file))
+            self.data.append((clean_stft, noisy_stft))
+
     def __len__(self):
-        return len(self.noisy_files)
+        return len(self.data)
     
     def __getitem__(self, idx):
-        clean_file = self.clean_files[idx]
-        noisy_file = self.noisy_files[idx]
+        return self.data[idx]
 
-        clean_stft = torch.load(os.path.join(self.clean_path, clean_file))
-        noisy_stft = torch.load(os.path.join(self.noisy_path, noisy_file))
-
-        return clean_stft, noisy_stft
-
-# Lightning DataModule
 class VCTKDataModule(L.LightningDataModule):
     def __init__(self, clean_path, noisy_path, test_clean_path, test_noisy_path, batch_size=16, num_workers=16):
         super(VCTKDataModule, self).__init__()
@@ -39,18 +40,88 @@ class VCTKDataModule(L.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
             self.vctk_train = AudioDataset(self.clean_path, self.noisy_path)
             self.vctk_val = AudioDataset(self.test_clean_path, self.test_noisy_path)
-            
 
     def train_dataloader(self):
         return DataLoader(self.vctk_train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, persistent_workers=True)
     
     def val_dataloader(self):
         return DataLoader(self.vctk_val, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, persistent_workers=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # no caching:
+# class AudioDataset(Dataset):
+#     def __init__(self, clean_path, noisy_path, standardize=None,
+#                  new_sample_rate=16000):
+#         super(AudioDataset, self).__init__()
+#         self.clean_path = clean_path
+#         self.clean_files = [file for file in os.listdir(clean_path) if file.endswith('.pt')]
+
+#         self.noisy_path = noisy_path
+#         self.noisy_files = [file for file in os.listdir(noisy_path) if file.endswith('.pt')]
+
+#         self.new_sample_rate = new_sample_rate
+#         self.standardize = standardize
+
+#     def __len__(self):
+#         return len(self.noisy_files)
+    
+#     def __getitem__(self, idx):
+#         clean_file = self.clean_files[idx]
+#         noisy_file = self.noisy_files[idx]
+
+#         clean_stft = torch.load(os.path.join(self.clean_path, clean_file))
+#         noisy_stft = torch.load(os.path.join(self.noisy_path, noisy_file))
+
+#         return clean_stft, noisy_stft
+
+# # Lightning DataModule
+# class VCTKDataModule(L.LightningDataModule):
+#     def __init__(self, clean_path, noisy_path, test_clean_path, test_noisy_path, batch_size=16, num_workers=16):
+#         super(VCTKDataModule, self).__init__()
+#         self.clean_path = clean_path
+#         self.noisy_path = noisy_path
+#         self.test_clean_path = test_clean_path
+#         self.test_noisy_path = test_noisy_path
+#         self.batch_size = batch_size
+#         self.num_workers = num_workers
+
+
+#     def setup(self, stage=None):
+#         if stage == 'fit' or stage is None:
+#             self.vctk_train = AudioDataset(self.clean_path, self.noisy_path)
+#             self.vctk_val = AudioDataset(self.test_clean_path, self.test_noisy_path)
+            
+
+#     def train_dataloader(self):
+#         return DataLoader(self.vctk_train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, persistent_workers=True)
+    
+#     def val_dataloader(self):
+#         return DataLoader(self.vctk_val, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, persistent_workers=True)
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
