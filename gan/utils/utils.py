f424f34b888ea41fa316f7557a840e221b1d16e3
Frederikravnborg <frederik@ravnborg.fr>
2024-03-18 15:11:35 +0100

    Update utils.py

--- gan/utils/utils.py
+++ gan/utils/utils.py
@@ -19,33 +19,32 @@ def compute_scores(real_clean_waveform, fake_clean_waveform, non_matching_refere
     if len(non_matching_reference_waveform.numpy().shape) == 1:
         non_matching_reference_waveform = non_matching_reference_waveform.unsqueeze(0)
 
-    # ## SI-SNR
-    # sisnr = ScaleInvariantSignalNoiseRatio()
-    # sisnr_score = sisnr(preds=fake_clean_waveform, target=real_clean_waveform)
+    ## SI-SNR
+    sisnr = ScaleInvariantSignalNoiseRatio()
+    sisnr_score = sisnr(preds=fake_clean_waveform, target=real_clean_waveform)
 
-    # ## DNSMOS
-    # dnsmos_score = dnsmos.run(fake_clean_waveform.numpy(), 16000)['ovrl_mos']
+    ## DNSMOS
+    dnsmos_score = dnsmos.run(fake_clean_waveform.numpy(), 16000)['ovrl_mos']
 
-    # ## MOS Squim
-    # subjective_model = SQUIM_SUBJECTIVE.get_model()
-    # mos_squim_score = subjective_model(fake_clean_waveform.unsqueeze(0), non_matching_reference_waveform)
+    ## MOS Squim
+    subjective_model = SQUIM_SUBJECTIVE.get_model()
+    mos_squim_score = subjective_model(fake_clean_waveform.unsqueeze(0), non_matching_reference_waveform)
         
     ## eSTOI
     estoi = ShortTimeObjectiveIntelligibility(16000, extended = True)
     estoi_score = estoi(preds = fake_clean_waveform, target = real_clean_waveform)
 
-    # ## PESQ Normal
-    # pesq_normal_score = pesq(fs=16000, ref=real_clean_waveform.numpy(), deg=fake_clean_waveform.numpy(), mode='wb')
+    ## PESQ Normal
+    pesq_normal_score = pesq(fs=16000, ref=real_clean_waveform.numpy(), deg=fake_clean_waveform.numpy(), mode='wb')
 
-    # ## PESQ Torch
-    # pesq_torch = PerceptualEvaluationSpeechQuality(fs=16000, mode='wb')
-    # pesq_torch_score = pesq_torch(real_clean_waveform, fake_clean_waveform)
+    ## PESQ Torch
+    pesq_torch = PerceptualEvaluationSpeechQuality(fs=16000, mode='wb')
+    pesq_torch_score = pesq_torch(real_clean_waveform, fake_clean_waveform)
 
     ## Predicted objective metrics: STOI, PESQ, and SI-SDR
     objective_model = SQUIM_OBJECTIVE.get_model()
     stoi_pred, pesq_pred, si_sdr_pred = objective_model(fake_clean_waveform.unsqueeze(0))
 
-    return 0, 0, 0, estoi_score.item(), 0, 0, stoi_pred.item(), pesq_pred, si_sdr_pred.item()
     return sisnr_score.item(), dnsmos_score, mos_squim_score.item(), estoi_score.item(), pesq_normal_score, pesq_torch_score.item(), stoi_pred.item(), pesq_pred.item(), si_sdr_pred.item()
 
 def perfect_shuffle(tensor):
