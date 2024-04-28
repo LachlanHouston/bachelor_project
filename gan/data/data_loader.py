3a8302b15a0bacf43aba75a808ec10cbdc47df62
Frederikravnborg <frederik@ravnborg.fr>
2024-04-28 20:47:53 +0200

    gpu fix

--- gan/data/data_loader.py
+++ gan/data/data_loader.py
@@ -115,9 +115,7 @@ class AudioDataset(Dataset):
         # Stack the real and imaginary parts of the STFT
         clean_stft = torch.stack((clean_stft.real, clean_stft.imag), dim=1)
         noisy_stft = torch.stack((noisy_stft.real, noisy_stft.imag), dim=1)
-        if torch.cuda.is_available():
-            device = torch.device('cuda')
-            return clean_stft.squeeze().to(device), noisy_stft.squeeze().to(device)
+
         return clean_stft.squeeze(), noisy_stft.squeeze() #, self.clean_files[clean_idx], self.noisy_files[idx]
 
 
